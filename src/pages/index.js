77792edba606fe98c432967f4b6adf76e23231ed
jcsmillie <jonnysmillie@jonnysmillie.co.uk>
2021-08-06 12:43:34 +0100

    update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,6 @@ const IndexPage = () => (
       <div className="col-12 mt-5 d-flex justify-content-center">
         <div className="box">
           <div className="box-body">
-            <p>Hola amingos</p>
               <StaticImage
               src="../images/conchal.jpeg"
               width={400}
@@ -39,6 +38,9 @@ const IndexPage = () => (
 </div>
 <div style={{ maxWidth: `960px`, margin: `2.45rem`, marginTop: `18rem` }}>
     <h2 className='text-center'>{JSONData.title}</h2>
+    <p>Elegant and exclusive, our Royal Beach Club Suite will complement your dreamed stay. Located within a separated area in our resort, where you will enjoy access to an adults-only pool and lounge, personalized concierge/butler service and premium alcohol beverages as part of your benefits.</p>
+    <p>This suite will offer you the private and relaxed touch you want for your stay. As soon as you arrive, you will feel invited to settle in and relax, take a complimentary Costa Rican coffee to the balcony, featuring an outdoor soaking tub. Along with refreshing garden or pool views, this suite is equipped with a 32-inch LCD TV, an iPod docking station and complimentary High Speed Internet Access.</p>
+    <p>In addition, its modern and stylish bathroom off from the living area, which provides extra privacy and convenience, is furnished with an invigorating rainforest shower, Westin White Tea Heavenly™ Bath Amenities, plush bathrobes, and slippers to complete a unique experience. Royal Beach Club Suites are limited to guests aged 18 and over.</p>
     <div className="messages-container"></div>
     <ul className="grid-testimonials">
       {JSONData.content.map((data, index) => {
